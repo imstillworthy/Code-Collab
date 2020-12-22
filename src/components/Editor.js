efa8412d123fbe0c0f4fd0ea95a1879c6dc92cfc
Yash Gupta <guptayash1104@gmail.com>
2020-12-23 00:02:21 +0530

    Upload File

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,13 +1,14 @@
 
 import React, { useState, useRef } from "react";
 import Editor from "@monaco-editor/react";
-import { Dropdown, ButtonGroup, Button } from 'react-bootstrap';
+import { Dropdown, Button, Alert, Badge } from 'react-bootstrap';
 import Nav from "./Navbar";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Area() {
   const theme = "dark";
   const [isEditorReady, setIsEditorReady] = useState("false");
   const [language, setLanguage] = useState("javascript");
+  const [editorData, setData] = useState()
   const valueGetter = useRef();
 
 
@@ -16,61 +17,85 @@ function Area() {
     valueGetter.current = _valueGetter;
   }
 
-  function extension(language){
-    switch(language){
+  function extension(language) {
+    switch (language) {
       case 'cpp':
         return 'cpp'
-      case 'java' :
+      case 'java':
         return 'java'
-      case 'javascript' :
+      case 'javascript':
         return 'js'
-      case 'python' :
+      case 'python':
         return 'py'
-      case 'c' :
+      case 'c':
         return 'c'
-      default :
+      default:
         return 'txt'
     }
   }
 
-  function handleDownload(){
+  function handleDownload() {
     const element = document.createElement("a");
-    const file = new Blob([valueGetter.current()],    
-                {type: 'text/plain;charset=utf-8'});
+    const file = new Blob([valueGetter.current()],
+      { type: 'text/plain;charset=utf-8' });
     element.href = URL.createObjectURL(file);
-    element.download = "myFile."+extension(language);
+    element.download = "myFile." + extension(language);
     document.body.appendChild(element);
     element.click();
+  }
+
+  function fileUpload(e) {
+    let file = e.target.files
+    let reader = new FileReader()
+    reader.readAsText(file[0])
+    reader.onload = e => setData(e.target.result)
+    e.target.style.visibility = "hidden"
   }
   return (
     <div>
       <Nav />
-      <div className="container" style={{ marginBottom: "1rem"}}>
-        <h1 style={{ textAlign: "center" }}>Main Editor</h1>
-        <Dropdown>
-          <Dropdown.Toggle id="dropdown-basic" variant="warning" style={{ backgroundColor: "white", marginBottom: "1rem", marginLeft: "0" }}>
-            {language === "cpp" ? "c++" : language}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item onClick={() => { setLanguage("c") }}>C</Dropdown.Item>
-            <Dropdown.Item onClick={() => { setLanguage("cpp") }}>C++</Dropdown.Item>
-            <Dropdown.Item onClick={() => { setLanguage("java") }}>Java</Dropdown.Item>
-            <Dropdown.Item onClick={() => { setLanguage("javascript") }}>Javascript</Dropdown.Item>
-            <Dropdown.Item onClick={() => { setLanguage("python") }}>Python</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+      <div className="container" style={{ marginBottom: "1rem" }}>
+        {/* <h1 style={{ textAlign: "center" }}>Main Editor</h1> */}
+        <div style={{ textAlign: "center" }}>
+          <Badge pill variant="warning">
+            Select Language
+          </Badge>{' '}
+          <Dropdown style={{ justifyContent: "center", alignItems: "center" }}>
+            <Dropdown.Toggle id="dropdown-basic" variant="warning" style={{
+              backgroundColor: "white", marginBottom: "1rem", marginLeft: "0"
+            }}>
+              {language === "cpp" ? "c++" : language}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => { setLanguage("c") }}>C</Dropdown.Item>
+              <Dropdown.Item onClick={() => { setLanguage("cpp") }}>C++</Dropdown.Item>
+              <Dropdown.Item onClick={() => { setLanguage("java") }}>Java</Dropdown.Item>
+              <Dropdown.Item onClick={() => { setLanguage("javascript") }}>Javascript</Dropdown.Item>
+              <Dropdown.Item onClick={() => { setLanguage("python") }}>Python</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+
+        <Alert variant='dark' style={{ fontSize: "75%", textAlign: "center" }}>You can either choose to upload a file and
+        then work on it or directly start coding in the editor below!</Alert>
+
+        <input button type="file" accept=".py , .cpp , .c , .java , .js, .txt" style={{
+          marginTop: "1rem", marginBottom: "2rem", marginLeft: "auto", marginRight: "auto"
+        }} name="file" onChange={e => fileUpload(e)} />
 
         <Editor
           height="75vh"
           theme={theme}
           language={language}
           editorDidMount={handleEditorDidMount}
-          options={{ lineNumbers: "on",
-          automaticLayout: "true",
+          value={editorData}
+          options={{
+            lineNumbers: "on",
+            automaticLayout: "true",
           }} />
 
-        <Button variant="Primary" onClick={handleDownload} style={{ marginTop: "1rem", backgroundColor: "blue", color: "white" }}>Download File</Button>
-        <Button varaint="Primary" style={{ marginTop: "1rem", marginLeft: "1rem", backgroundColor: "black" }}>Open File</Button>
+        <Button block variant="info" onClick={handleDownload} style={{ marginTop: "1rem", color: "white" }}>Download File</Button>
+
       </div>
 
     </div>
