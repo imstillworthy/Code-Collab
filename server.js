9c393b947666503a7b4292e49b27ee9fd3e49dd9
Omanshu Mahawar <omanshumahawar.181co237@nitk.edu.in>
2021-03-02 17:42:14 +0530

    adding production configuration for deploying

--- server.js
+++ server.js
@@ -60,4 +60,12 @@ io.on("connection", (socket) => {
     })
 });
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('Client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'Client','build','index.html'))
+    })
+}
+
 server.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
